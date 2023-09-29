ced6a4b63eaaebd02d9b8df0d90dc10e62daefb0
Shujaa <shujaa.oduol@gmail.com>
2023-09-29 17:35:01 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-function addingEventListener() {
+const input = document.getElementById('input');
+
+function addingEventListener(){
+  input.addEventListener('click', function addEventListener() {
+    alert('I was clicked!');
+  })
 }
+
+input.addEventListener('click', true);
